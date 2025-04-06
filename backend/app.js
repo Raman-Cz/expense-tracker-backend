fa6e8096517c6b0ae214e0acd0444109d2640171
Raman-Cz <ramankemwal@gmail.com>
2025-04-06 22:31:24 +0530

    fixed url origin

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 //middlewares
 app.use(express.json())
 app.use(cors({
-    origin:"https://expense-tracker-frontend-sage.vercel.app/",
+    origin:"https://expense-tracker-frontend-sage.vercel.app",
     credentials:true
 }))
 
